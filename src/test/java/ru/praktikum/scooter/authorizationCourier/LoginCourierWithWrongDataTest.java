9a97260d3c1f4befc792c78f9b29023823c95240
klirovskij <135542945+klirovskij@users.noreply.github.com>
2023-11-10 21:33:45 +0300

    Update LoginCourierWithWrongDataTest.java

--- src/test/java/ru/praktikum/scooter/authorizationCourier/LoginCourierWithWrongDataTest.java
+++ src/test/java/ru/praktikum/scooter/authorizationCourier/LoginCourierWithWrongDataTest.java
@@ -1,52 +1,38 @@
-package ru.praktikum.scooter.authorizationCourier;
+package ru.praktikum.scooter.creatingCourier;
 
-import io.qameta.allure.Description;
 import io.qameta.allure.junit4.DisplayName;
 import io.restassured.response.ValidatableResponse;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import ru.praktikum.scooter.courier.Courier;
 import ru.praktikum.scooter.courier.CourierClient;
 import ru.praktikum.scooter.courier.CourierParams;
 
-import static org.hamcrest.CoreMatchers.equalTo;
+import static org.apache.http.HttpStatus.SC_BAD_REQUEST;
+import static org.junit.Assert.assertEquals;
 
-@RunWith(Parameterized.class)
-public class LoginCourierWithWrongDataTest {
+public class CreateCourierWithoutPasswordTest {
     private Courier courier;
     private CourierClient courierClient;
-    private int statusCode;
-    private final String error;
 
-    public LoginCourierWithWrongDataTest(String error, int statusCode, Courier courier) {
-        this.error = error;
-        this.courier = courier;
-        this.statusCode = statusCode;
-    }
-
-    @Parameterized.Parameters
-    public static Object[][] getDataForTryingLogin() {
-        return new Object[][]{
-                {"Учетная запись не найдена", 404, new Courier("КурьерДляТеста", "Qwerty123", "Тестировочный")},
-                {"Учетная запись не найдена", 404, new Courier("КурьерДляТестаСОпечаткойАупввв", "Qwerty", "Тестировочный")},
-                {"Недостаточно данных для входа", 400, new Courier("КурьерДляТеста", null, "Тестировочный")},
-                {"Недостаточно данных для входа", 400, new Courier(null, "Qwerty", "Тестировочный")},
-        };
-    }
+    private String errorMessageActual;
+    private String errorMessageExpected = "Недостаточно данных для создания учетной записи";
 
     @Before
     public void setUp() {
+        courier = CourierParams.randomCourierNoPassword();
         courierClient = new CourierClient();
     }
 
+
     @Test
-    @DisplayName("Пробуем войти под курьером с невалидными или недостаточными данными")
-    @Description("Проверяем, что код ответа и сообщение об ошибке соответствуют документации")
-    public void tryToAuthCourier() {
-        ValidatableResponse validatableResponse = courierClient.loginCourier(courier)
-                .assertThat().statusCode(statusCode).log().all()
-                .and().body("message", equalTo(error);
+    @DisplayName("Пробуем создать курьера не указывая пароль ")
+    public void toCreateCourierWithoutPassword() {
+        ValidatableResponse validatableResponse =
+                courierClient.createCourier(courier);
+        errorMessageActual = validatableResponse.extract().path("message");
+        validatableResponse.assertThat().statusCode(SC_BAD_REQUEST);
+        assertEquals(errorMessageExpected,errorMessageActual);
+
     }
 }
