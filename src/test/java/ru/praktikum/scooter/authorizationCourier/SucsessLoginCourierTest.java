79f5e4b3b26d498a31e3e52343af512bbaf713f8
klirovskij <135542945+klirovskij@users.noreply.github.com>
2023-11-10 21:30:32 +0300

    Update SucsessLoginCourierTest.java

--- src/test/java/ru/praktikum/scooter/authorizationCourier/SucsessLoginCourierTest.java
+++ src/test/java/ru/praktikum/scooter/authorizationCourier/SucsessLoginCourierTest.java
@@ -2,7 +2,8 @@ package ru.praktikum.scooter.authorizationCourier;
 
 import io.qameta.allure.junit4.DisplayName;
 import io.restassured.response.ValidatableResponse;
-import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.AfterClass;
 import org.junit.Test;
 import ru.praktikum.scooter.courier.Courier;
 import ru.praktikum.scooter.courier.CourierClient;
@@ -12,24 +13,27 @@ import static org.apache.http.HttpStatus.SC_OK;
 import static org.hamcrest.CoreMatchers.equalTo;
 
 public class SucsessLoginCourierTest {
-    private Courier courier;
-    private CourierClient courierClient;
-    private static Integer CREATED_COURIER_ID = 229718;
-    @Before
-    public void setUp() {
-        courier = CourierParams.createdCourier();
+    private static CourierClient courierClient;
+
+    @BeforeClass
+    public static void setUp() {
         courierClient = new CourierClient();
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        // Здесь можно удалить курьера или выполнить другие действия по завершению всех тестов.
     }
 
     @Test
     @DisplayName("Заходим под ранее созданным курьером")
     public void authtorizationCreatedCourier() {
+        // Создаем случайного курьера для этого теста
+        Courier courier = CourierParams.createdCourier();
 
         ValidatableResponse validatableResponse =
                 courierClient.loginCourier(courier)
-                        .assertThat().statusCode (SC_OK)
-                        .and().body("id", equalTo(229718));
-
+                        .assertThat().statusCode(SC_OK)
+                        .and().body("id", equalTo(courier.getId()));
     }
-
 }
