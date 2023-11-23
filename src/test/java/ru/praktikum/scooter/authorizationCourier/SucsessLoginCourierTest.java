ce8072d64f4173f40fc88ffe7460c2dd23240b02
klirovskij <135542945+klirovskij@users.noreply.github.com>
2023-11-23 19:20:58 +0300

    Update SucsessLoginCourierTest.java

--- src/test/java/ru/praktikum/scooter/authorizationCourier/SucsessLoginCourierTest.java
+++ src/test/java/ru/praktikum/scooter/authorizationCourier/SucsessLoginCourierTest.java
@@ -1,11 +1,15 @@
 package ru.praktikum.scooter.authorizationCourier;
 
+import io.qameta.allure.Description;
 import io.qameta.allure.junit4.DisplayName;
+import io.restassured.response.ValidatableResponse;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import ru.praktikum.scooter.courier.Courier;
 import ru.praktikum.scooter.courier.CourierClient;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
 
 public class SucsessLoginCourierTest {
     private static CourierClient courierClient;
@@ -27,7 +31,10 @@ public class SucsessLoginCourierTest {
     }
 
     @Test
-    @DisplayName("Заходим под ранее созданным курьером")
-    public void authtorizationCreatedCourier() {
+    @DisplayName("Выполняем вход под созданным курьером")
+    @Description("Проверяем успешный вход под созданным курьером")
+    public void sucsessLoginCourierTest() {
+        ValidatableResponse loginResponse = courierClient.loginCourier(createdCourier);
+        loginResponse.assertThat().statusCode(200).body("id", notNullValue());
     }
 }
